ddd39851a6473838e0195e7e1af25ab98d61e1d9
Martin Salazard <martin.der.aze@gmail.com>
2017-05-29 22:49:00 +0200

    Add some more tests

---
+++ package/src/test/resources/gradle/packaged-project/src/bar.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+printBar() {
+	echo "bar-bar"
+}
+
+printBar
---
+++ package/src/test/resources/gradle/packaged-project/src/foo.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+# @description print 'foo-foo'
+printFoo() {
+	echo "foo-foo"
+}
+
+printFoo
