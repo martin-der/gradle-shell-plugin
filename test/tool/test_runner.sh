132a84cc2cb97a138a25260be40c5895038941b9
Martin Salazard <martin.der.aze@gmail.com>
2020-05-28 01:35:42 +0200

    Test runner : clean

--- test/tool/test_runner.sh
+++ test/tool/test_runner.sh
@@ -7,8 +7,8 @@ mkTestResultsDir() {
 }
 
 runTests() {
-	source "${MDU_SHELLTEST_TESTUNIT_SHUNIT2_EXEC}" || exit $MDU_SHELLTEST_TEST_EXECUTION_ERROR_EXIT_CODE
-	[ $__shunit_testsFailed -gt 0 ] && exit $MDU_SHELLTEST_TEST_ASSERTION_FAILURE_EXIT_CODE || exit 0
+	source "${MDU_SHELLTEST_TESTUNIT_SHUNIT2_EXEC}" || exit ${MDU_SHELLTEST_TEST_EXECUTION_ERROR_EXIT_CODE}
+	[ ${__shunit_testsFailed} -gt 0 ] && exit ${MDU_SHELLTEST_TEST_ASSERTION_FAILURE_EXIT_CODE} || exit 0
 }
 
 
