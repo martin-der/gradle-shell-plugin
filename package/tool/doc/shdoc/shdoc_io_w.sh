822802c2196a6edd918525af40d79660b108468e
Martin Salazard <martin.der.aze@gmail.com>
2020-05-28 17:30:11 +0200

    Remove duplicate methods in shdoc and fix Converter

--- package/tool/doc/shdoc/shdoc_io_w.sh
+++ package/tool/doc/shdoc/shdoc_io_w.sh
@@ -2,8 +2,11 @@
 
 set -u
 
+GAWK_COMMAND=gawk
+test "x$3" != x && GAWK_COMMAND="$3"
+
 SHDOC="$(dirname "$0")/shdoc"
 
 FILENAME="$(basename "$1")"
 
-cat "$1" | "$SHDOC" -v script_name="$FILENAME" > "$2"
+cat "$1" | "${GAWK_COMMAND}" -f "$SHDOC" -v script_name="$FILENAME" > "$2"
