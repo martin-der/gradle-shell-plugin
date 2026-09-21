af23ad5af48057ee44ad2fd74100a98d3bea69f0
A.L.T.U.R <altur@example.net>
2026-09-21 18:03:28 +0000

    Making executable in windows a no-op

--- shell-package/src/main/java/net/tetrakoopa/gradle/SystemUtil.java
+++ shell-package/src/main/java/net/tetrakoopa/gradle/SystemUtil.java
@@ -10,12 +10,32 @@ import java.nio.file.attribute.PosixFileAttributes;
 import java.nio.file.attribute.PosixFilePermission;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.logging.Logger;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class SystemUtil {
-	
+
+	public static boolean isWindows() {
+		return System.getProperty("os.name").toLowerCase().contains("win");
+	}
+
+	private static boolean windowsExecutableWarningPrinted = false;
+
+	private static void warnWindowsExecutableNoOp() {
+		if (!windowsExecutableWarningPrinted) {
+			windowsExecutableWarningPrinted = true;
+			Logger.getLogger(SystemUtil.class.getName()).warning(
+				"Running on Windows: makeExecutable is a no-op, file permissions cannot be set."
+			);
+		}
+	}
+
 	public static void makeExecutable(File file, boolean toGroup, boolean toOther) throws IOException {
+		if (isWindows()) {
+			warnWindowsExecutableNoOp();
+			return;
+		}
 		final Path path = file.toPath();
 
 		final Set<PosixFilePermission> permissions = Files.readAttributes(path,PosixFileAttributes.class).permissions();
